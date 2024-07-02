3b0b7a2b20025e8ac9fda06aeae4d6c4b6bbae4f
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-02 15:18:40 +0700

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'altomedia'
+    },
+    whatsappApiKey: process.env.WHATSAPP_API_KEY,
+    qrCodeUrl: process.env.QR_CODE_URL
+};
